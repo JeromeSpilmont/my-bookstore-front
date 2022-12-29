e645facdf12b96bb3d8ca70306d62b8dcdbec98f
Jerome S <jerome@student.be>
2022-12-29 16:01:21 +0100

    fin de premiere version du front

---
+++ src/app/services/books.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../domain/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//service
+export class BooksService {
+
+  URL= 'http://localhost:8080/books';
+
+  constructor(private http: HttpClient) { }
+
+
+  getAllBooks():Observable<Book[]>{
+    return this.http.get<Book[]>(this.URL);
+  }
+
+
+
+}
